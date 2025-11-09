55a1d0894afd5e805c1bf5263722f223397af0e8
Katari <kataricoder@gmail.com>
2025-11-10 01:05:05 +0530

    small change

--- backend/routes/AuthRoute.js
+++ backend/routes/AuthRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 
-router.post('/addEmployee', AuthenticateMiddleware, authorize('TENANT_ADMIN', 'HR'), addEmployee);
+router.post('/addEmployee', AuthenticateMiddleware, authorize('ADMIN', 'HR'), addEmployee);
 router.post('/setPassword', setEmployeePassword);
 router.post('/employeeLogin', employeeLogin);
 
