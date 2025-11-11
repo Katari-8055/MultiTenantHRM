bc3c6b4747fd8c8c44fac8029b8fa981797c64ad
Katari <kataricoder@gmail.com>
2025-11-11 22:38:37 +0530

    Adding getDepartment route

--- backend/Controllers/AdminComtroller.js
+++ backend/Controllers/AdminComtroller.js
@@ -2,6 +2,8 @@ import { asyncHandler } from "../utils/AsyncHandler.js";
 import prisma from "../utils/client.js";
 
 
+//-------------------------------------Add Department-----------------------------------//
+
 export const addDepartment = asyncHandler(async (req, res, next) => {
     const { name } = req.body;
     const tenantId = req.tenantId;
@@ -21,4 +23,20 @@ export const addDepartment = asyncHandler(async (req, res, next) => {
         department
     });
     
+})
+
+
+//----------------------------------------------Get Department------------------------------------//
+
+export const getDepartment = asyncHandler(async (req, res, next) => {
+    const tenantId = req.tenantId;
+    const departments = await prisma.department.findMany({
+        where: {
+            tenantId
+        }
+    });
+    res.status(200).json({
+        success: true,
+        departments
+    });
 })
--- backend/routes/AdminRoute.js
+++ backend/routes/AdminRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { AuthenticateMiddleware } from '../middlewares/AuthMiddleware.js';
-import { addDepartment } from '../Controllers/AdminComtroller.js';
+import { addDepartment, getDepartment } from '../Controllers/AdminComtroller.js';
 
 
 const router = express.Router();
 
 router.post('/addDepartment',AuthenticateMiddleware, addDepartment);
+router.get('/getDepartment',AuthenticateMiddleware,getDepartment);
 
 
 export default router;
