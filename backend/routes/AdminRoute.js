dfe7976aa65aa8a30e034287d2fb82290dcd7612
Katari <kataricoder@gmail.com>
2025-12-01 15:11:21 +0530

    making changes

--- backend/routes/AdminRoute.js
+++ backend/routes/AdminRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { AuthenticateMiddleware } from '../middlewares/AuthMiddleware.js';
-import { addDepartment, getDepartment, getEmployee } from '../Controllers/AdminComtroller.js';
+import { addDepartment, addProject, getDepartment, getEmployee, getProject } from '../Controllers/AdminComtroller.js';
 
 
 const router = express.Router();
@@ -8,6 +8,8 @@ const router = express.Router();
 router.post('/addDepartment',AuthenticateMiddleware, addDepartment);
 router.get('/getDepartment',AuthenticateMiddleware,getDepartment);
 router.get('/getEmployee',AuthenticateMiddleware,getEmployee);
+router.post('/addProject',AuthenticateMiddleware, addProject);
+router.get('/getProject',AuthenticateMiddleware,getProject);
 
 
 
